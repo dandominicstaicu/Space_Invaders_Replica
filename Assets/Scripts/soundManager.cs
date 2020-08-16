f697e64e27face94a4eb230fb7b4859b5f5da369
Dominic <dando.ds11@gmail.com>
2020-08-16 15:40:10 +0300

    First part of the game. Added player. Added sound manager.

---
+++ Assets/Scripts/soundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+    public static soundManager Instance = null;
+
+    public AudioClip AlienBuzz1;
+    public AudioClip AlienBuzz2;
+    public AudioClip AlienDies;
+    public AudioClip BulletFire;
+    public AudioClip ShipExplosion;
+
+    private AudioSource soundEffectAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        AudioSource theSource = GetComponent<AudioSource>();
+        soundEffectAudio = theSource;
+    }
+
+    public void playOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+}
