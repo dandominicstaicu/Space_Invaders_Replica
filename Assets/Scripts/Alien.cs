f54db7591c28206ff95010847b2c33d96ec7eb61
Dominic <dando.ds11@gmail.com>
2020-08-16 21:58:45 +0300

    Finished base game! Added alien bullets. Added script for aliens. Aliens moving and shooting.

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -4,15 +4,124 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 10;
+
+    private Rigidbody2D rigidBody;
+
+    public Sprite startingImage;
+    public Sprite altImage;
+
+    private SpriteRenderer spriteRenderer;
+
+    public float secBeforeSpriteChange = 0.5f;
+
+    public GameObject alienBullet;
+
+    public float minFireRateTime = 1.0f;
+    public float maxFireRateTime = 3.0f;
+    public float baseFireWaitTime = 3.0f;
+
+    public Sprite explodedShipImage;
+
     void Start()
     {
+
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        rigidBody.velocity = new Vector2(1, 0) * speed;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        StartCoroutine(changeAlienSprite());
         
+        baseFireWaitTime = baseFireWaitTime +
+            Random.Range(minFireRateTime, maxFireRateTime);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Turn(int direction)
     {
-        
+        Vector2 newVelocity = rigidBody.velocity;
+        newVelocity.x = speed * direction;
+        rigidBody.velocity = newVelocity;
     }
+
+    void MoveDown()
+    {
+        Vector2 position = transform.position;
+        position.y -= 1;
+        transform.position = position;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "leftWall")
+        {
+            Turn(1);
+            MoveDown();
+        }
+        if (col.gameObject.name == "rightWall")
+        {
+            Turn(-1);
+            MoveDown();
+        }
+
+        if (col.gameObject.tag == "Bullet")
+        {
+            soundManager.Instance.playOneShot(soundManager.Instance.AlienDies);
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    public IEnumerator changeAlienSprite()
+    {
+        while (true)
+        {
+            if (spriteRenderer.sprite == startingImage)
+            {
+                spriteRenderer.sprite = altImage;
+                soundManager.Instance.playOneShot(soundManager.Instance.AlienBuzz1);
+            }
+            else
+            {
+                spriteRenderer.sprite = startingImage;
+                soundManager.Instance.playOneShot(soundManager.Instance.AlienBuzz2);
+            }
+
+            yield return new WaitForSeconds(secBeforeSpriteChange);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Time.time > baseFireWaitTime)
+        {
+            baseFireWaitTime = baseFireWaitTime +
+                Random.Range(minFireRateTime, maxFireRateTime);
+
+            Instantiate(alienBullet, transform.position, Quaternion.identity);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            soundManager.Instance.playOneShot(soundManager.Instance.ShipExplosion);
+
+            col.GetComponent<SpriteRenderer>().sprite = explodedShipImage;
+
+            Destroy(gameObject);
+
+            DestroyObject(col.gameObject, 0.5f);
+        }
+    }
+
+
 }
